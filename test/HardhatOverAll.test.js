cf15717850ec3d57f054d3b8322bd633d2b4ba70
LazyCow <123736479+nft-dna@users.noreply.github.com>
2024-06-02 23:47:20 +0200

    Update HardhatOverAll.test.js

--- test/HardhatOverAll.test.js
+++ test/HardhatOverAll.test.js
@@ -1,6 +1,5 @@
 // npx hardhat test ./test/HardhatOverAll.test.js --network localhost; 
 // run first (in another shell): npx hardhat node
-require('@nomiclabs/hardhat-truffle5');
 
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
@@ -15,23 +14,29 @@ async function getGasCosts(receipt) {
     return gasPrice.mul(new BN(receipt.receipt.gasUsed));
 }    
 
-const MockERC20 = artifacts.require('MockERC20');
-
 describe("Overall Test", function () {	
 		
+	let MockERC20;
+	let token;
+	let owner;
+	let addr1;
+	let addr2;
+	let initialSupply = ethers.parseEther("1000");
+  
 	beforeEach(async function () {
-		this.owner = await ethers.getSigners();		
-		this.token = await MockERC20.new("MockToken", "MTK", 18, ethers.parseEther("1000"));
-		await this.token.waitForDeployment();
-		console.log(await this.token.getAddress());	
+		[owner, addr1, addr2] = await ethers.getSigners();	
+		MockERC20 = await ethers.getContractFactory("MockERC20");
+		token = await MockERC20.deploy("MockToken", "MTK", 18, initialSupply);
+		await token.waitForDeployment();		
+		//console.log(await token.getAddress());	
 	});
 		
 	describe("MockERC20", async function () {
 	  it("Should return the right name and symbol", async function () {
-	  	console.log(await this.token.getAddress());
-		expect(await this.token.name()).to.equal("MockToken");
-		expect(await this.token.symbol()).to.equal("MTK");
-		expect(await this.token.balanceOf(this.owner.address)).to.equal(ethers.parseEther("1000"));
+	  	//console.log(await token.getAddress());
+		expect(await token.name()).to.equal("MockToken");
+		expect(await token.symbol()).to.equal("MTK");
+		expect(await token.balanceOf(owner.address)).to.equal(ethers.parseEther("1000"));
 	  });
 	});
 });
