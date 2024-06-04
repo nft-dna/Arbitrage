fa8d7160188f9d2f7c0e953295530e319f440e8f
LazyCow <123736479+nft-dna@users.noreply.github.com>
2024-06-05 00:57:39 +0200

    Update Trade.sol

--- contracts/Trade.sol
+++ contracts/Trade.sol
@@ -30,7 +30,7 @@ contract Trade is Deposit {
     function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
         require(tokenA != tokenB, 'IDENTICAL_ADDRESSES');
         (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        require(token0 != address(0), 'ZERO_ADDRESS');
+        //require(token0 != address(0), 'ZERO_ADDRESS');
     }
 	
 	function AddTestTokens(address[] calldata _tokens) external onlyOwner {
