1d9f965bdba9c977ac3b936d8b1a0731e952a282
LazyCow <123736479+nft-dna@users.noreply.github.com>
2024-06-05 01:22:35 +0200

    Update Trade.sol

--- contracts/Trade.sol
+++ contracts/Trade.sol
@@ -10,107 +10,17 @@ contract Trade is Deposit {
     address payable OWNER;
     address NATIVE_TOKEN = address(0x0);
 
-    address[] public dexAddresses; // Array to store dex addresses
-    mapping(address => DexInterfaceType) public dexInterface;    
-
     struct routeChain {
         address router;
         address asset;
         uint24 poolFee;
     }		
 	
-	event DualDexTraded(address indexed trader, address indexed _fromToken, address indexed _toToken, address _fromDex, address _toDex, uint256 _fromAmount, uint256 _gainedAmount);
-	event InstaTraded(address indexed trader, address indexed _baseAsset, routeChain[] _routeData, uint256 _fromAmount, uint256 _gainedAmount);
-
-    address [] public tokens;
-    address [] public stables;
-    // Mapping to store pools by token pairs
-    mapping(address => mapping(address => mapping(address => uint24))) public tokenV3PoolsFee;
-
-    function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
-        require(tokenA != tokenB, 'IDENTICAL_ADDRESSES');
-        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
-        //require(token0 != address(0), 'ZERO_ADDRESS');
-    }
-	
-	function AddTestTokens(address[] calldata _tokens) external onlyOwner {
-        for (uint i=0; i<_tokens.length; i++) {
-            tokens.push(_tokens[i]);
-        }
-    }
-
-    function AddTestStables(address[] calldata _stables) external onlyOwner {
-        for (uint i=0; i<_stables.length; i++) {
-            stables.push(_stables[i]);
-        }
-    }    
-
-    function AddTestV3PoolFee(address router, address token1, address token2, uint24 fee) external onlyOwner {
-		(address tokenA, address tokenB) = sortTokens(token1, token2);
-        tokenV3PoolsFee[router][tokenA][tokenB] = fee;
-    }
-	
-    function getTestV3PoolFee(address router, address token1, address token2) internal view returns (uint24) {
-		(address tokenA, address tokenB) = sortTokens(token1, token2);
-        return tokenV3PoolsFee[router][tokenA][tokenB];
-    }	
+    event DualDexTraded(address indexed trader, address indexed _fromToken, address indexed _toToken, address _fromDex, address _toDex, uint256 _fromAmount, uint256 _gainedAmount);
+    event InstaTraded(address indexed trader, address indexed _baseAsset, routeChain[] _routeData, uint256 _fromAmount, uint256 _gainedAmount);
 
     constructor() {
         OWNER = payable(msg.sender);
-    }
-
-    function IsDexAdded(address _dex) internal view returns (bool) {
-        for (uint256 i = 0; i < dexAddresses.length; i++) {
-            if (dexAddresses[i] == _dex) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    function AddDex(address[] calldata  _dex, DexInterfaceType[] calldata  _interface) public onlyOwner {
-        require ( _dex.length == _interface.length, "Invalid param");
-        for (uint i=0; i<_dex.length; i++) {
-            if (!IsDexAdded(_dex[i])) {
-                dexAddresses.push(_dex[i]);
-            }
-            dexInterface[_dex[i]] = _interface[i];
-        }
-    }
-
-    function getDexCount() public view returns (uint256) {
-        return dexAddresses.length;
-    }
-
-    function GetAmountOutMin(address router, uint24 poolfee, address _tokenIn, address _tokenOut, uint256 _amount) public view returns (uint256 ) {
-        if (dexInterface[router] == DexInterfaceType.IUniswapV3Router || poolfee > 0) {
-			require(NATIVE_TOKEN != _tokenIn, "Router does not support direct ETH swap");
-			require(NATIVE_TOKEN != _tokenOut, "Router does not support direct ETH swap");
-			if (poolfee >= 1000000) {
-				poolfee = 0;
-			}
-            uint256 result = IQuoter(router).quoteExactInputSingle(_tokenIn, _tokenOut , poolfee, _amount, 0);
-            return result;
-        } else {
-            uint256 result = 0;            
-            address[] memory path;
-            path = new address[](2);
-            path[0] = _tokenIn;
-            path[1] = _tokenOut;            
-            try IUniswapV2Router(router).getAmountsOut(_amount, path) returns (uint256[] memory amountOutMins) {
-                result = amountOutMins[path.length-1];
-            } catch {
-            }
-            return result;      			
-		//uint256[] memory amountOutMins = IUniswapV2Router(router).getAmountsOut(_amount, path);
-		//return amountOutMins[path.length-1];         
-        }
-    }
-
-    function EstimateDualDexTrade(address _fromToken, address _toToken, address _fromDex, uint24 _fromPoolFee, address _toDex, uint24 _toPoolFee, uint256 _fromAmount) external view returns (uint256) {
-        uint256 amtBack1 = GetAmountOutMin(_fromDex, _fromPoolFee, _fromToken, _toToken, _fromAmount);
-        uint256 amtBack2 = GetAmountOutMin(_toDex, _toPoolFee, _toToken, _fromToken, amtBack1);
-        return amtBack2;
     }
   
     function DualDexTrade(address _fromToken, address _toToken, address _fromDex, uint24 _fromPoolFee, address _toDex, uint24 _toPoolFee, uint256 _fromAmount, uint deadlineDeltaSec) payable public {
@@ -164,10 +74,10 @@ contract Trade is Deposit {
 		if (NATIVE_TOKEN != _tokenIn) {
 			IERC20(_tokenIn).approve(router, _amount);
 		}
-        if (dexInterface[router] == DexInterfaceType.IUniswapV3Router || _poolFee > 0) {
+        if (_poolFee > 0) {
 			require(NATIVE_TOKEN != _tokenIn, "Router does not support direct ETH swap");
 			require(NATIVE_TOKEN != _tokenOut, "Router does not support direct ETH swap");
-			if (_poolFee == 1) {
+			if (_poolFee == 100000) {
 				_poolFee = 0;
 			}		
             bytes memory params = abi.encode(
@@ -200,7 +110,7 @@ contract Trade is Deposit {
         }
     }    
 
-	function InstaTradeTokens(routeChain[] calldata _routedata, uint256 _startAmount, uint deadlineDeltaSec) payable public {
+    function InstaTradeTokens(routeChain[] calldata _routedata, uint256 _startAmount, uint deadlineDeltaSec) payable public {
 		require ( _routedata.length > 1, "Invalid param");
         
         uint256 startBalance = 0;
@@ -245,49 +155,7 @@ contract Trade is Deposit {
 		return tradeableAmount;
     }
 		
-    function AmountBack(
-        address router,
-        address baseAsset,
-        uint256 amount,
-        address token1,
-        address token2,
-        address token3
-    ) internal view returns (uint256) {
-        uint256 amtBack = GetAmountOutMin(router, getTestV3PoolFee(router, baseAsset, token1), baseAsset, token1, amount);
-        amtBack = GetAmountOutMin(router, getTestV3PoolFee(router, token1, token2), token1, token2, amtBack);
-        amtBack = GetAmountOutMin(router, getTestV3PoolFee(router, token2, token3), token2, token3, amtBack);
-        amtBack = GetAmountOutMin(router, getTestV3PoolFee(router, token3, baseAsset), token3, baseAsset, amtBack);
-        return amtBack;
-    }
-
-    // Base Asset > Altcoin > Stablecoin > Altcoin > Base Asset
-    function CrossStableSearch(address/*[] calldata _routers*/_router, address _baseAsset, uint256 _amount) external view returns (uint256,address,address,address) {
-        uint256 maxAmtBack = 0;
-        address token1;
-        address token2;
-        address token3;
-        //for (uint i0=0; i0<dexAddresses.length; i0++) {
-            for (uint i1=0; i1<tokens.length; i1++) {
-				if (_baseAsset != tokens[i1]) {
-					for (uint i2=0; i2<stables.length; i2++) {
-						for (uint i3=0; i3<tokens.length; i3++) {
-							if (_baseAsset != tokens[i3]) {
-								uint256 amtBack = AmountBack(_router, _baseAsset, _amount, tokens[i1], stables[i2], tokens[i3]);
-								if (amtBack > _amount && amtBack > maxAmtBack) {
-									maxAmtBack = amtBack;
-									token1 = tokens[i1];
-									token2 = tokens[i2];
-									token3 = tokens[i3];
-								}
-							}
-						}
-					}
-				}
-            }
-        //}
-        return (maxAmtBack,token1,token2,token3);
-    }   
-		
+	
     // Allow the contract to receive Ether
     receive () external payable  {
     
